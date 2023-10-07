e8acdafeabbf6214d322fcb64526a6a19b8a17a5
Pivi <pivi@pivis-air.home>
2023-10-07 14:22:49 +0200

    reset

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({path: '.env'})
 
 
 const Vimeo = require("vimeo").Vimeo;
