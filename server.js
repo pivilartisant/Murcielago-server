baf4c83c98d91ee3eb53ee81efb3dc985c01e637
Pivi <pivi@pivis-air.home>
2023-10-15 23:46:09 +0200

    Implement Caching

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const NodeCache = require('node-cache');
 const app = express();
+const cache = new NodeCache();
+
 
 require('dotenv').config({path: '.env'})
 
@@ -15,10 +17,6 @@ const client_id = process.env.VIMEO_CLIENT_ID;
 const client_secret = process.env.VIMEO_CLIENT_SECRET;
 
 const vimeoClient = new Vimeo(client_id, client_secret, access_token);
-
-// Enable CORS for all routes
-app.use(cors());
-
 // Apply rate limiting middleware
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
@@ -26,9 +24,10 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again later.",
 });
 
+app.use(cors());
 app.set("trust proxy", 1);
-
 app.use(limiter);
+
 
 
 
@@ -40,63 +39,49 @@ app.use((err, req, res, next) => {
       .json({ error: "Rate limit exceeded. Please try again later." });
   } else {
     // Handle other errors
-    res.status(500).json({ error: "Something went wrong." });
+    res.status(500).json({ error: "Something went wrong during rate limitation." });
   }
 });
-
-
 
 //Requests to vimeo API 
 app.get("/api/videos/:videoId", (req, res) => {
   const videoId = req.params.videoId;
-
-  vimeoClient.request(
+  const videoInfoCache = cache.get(videoId);
+  if (!videoId){
+    res.status(404)
+      .json({error : "Video ID not found"})
+  }
+  if (videoInfoCache) {
+    res.status(200)
+       .json({ data: videoInfoCache });
+  } else {
+    vimeoClient.request(
     {
       method: "GET",
       path: `/videos/${videoId}`,
     },
     function (error, body) {
-      if (error) {
+      if (error) { 
         console.log(error);
-        return res
-          .status(500)
-          .json({ error: "An error occurred while fetching video details." });
-      }
-
-
-      res.json(body);
-    }
-  );
-});
-
-//Requests to vimeo API 
-app.get("/api/pictures/:videoId", (req, res) => {
-  const videoId = req.params.videoId;
-
-  vimeoClient.request(
-    {
-      method: "GET",
-      path: `/videos/${videoId}`,
-    },
-    function (error, body) {
-      if (error) {
-        console.log(error);
-        return res
-          .status(500)
-          .json({ error: "An error occurred while fetching video details." });
-      }
-
-      // Check if the 'pictures' property exists in the response body
-      if (body && body.pictures) {
-        // Return the 'pictures' object in the response
-        return res.json(body.pictures);
+        if (res.statusCode === 500 ){
+            return res.status(500)
+                    .json({ error: "Request error" });
+        } else {
+          return res.status(501)
+                    .json({ error: "Unknown error "});
+        }}
+      if(body){
+        cache.set(videoId, body);
+         return res.status(200)
+                   .json(body);
       } else {
-        // If 'pictures' property is not found, return an appropriate response
-        return res.status(404).json({ error: "Pictures not found for this video." });
-      }
-    }
-  );
+        return res.status(404)
+                    .json({ error: "Content video information not found" });
+      }}
+    );
+  }
 });
+
 
 // Start the server
 const PORT = process.env.PORT || 8081;
